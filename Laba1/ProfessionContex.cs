6a4e9b1d32387dfa0dcdce6f9881a1728698fe9e
Cejurs <74813470+Cejurs@users.noreply.github.com>
2022-09-22 14:27:17 +0400

    Update ProfessionContex.cs

--- Laba1/ProfessionContex.cs
+++ Laba1/ProfessionContex.cs
@@ -15,7 +15,7 @@ namespace Laba1
         {
             optionsBuilder.UseMySql(
                 "server=localhost;user=root;password=password;database=professions;",
-                //"server=mysql5042.site4now.net;Uid=a8d23c_db;password=password1;database= db_a8d23c_db;",
+                //"server=mysql5042.site4now.net;Uid=a8d23c_db;password=password1;database= db_a8d23c_db;", // Удаленнаяя бд
                 new MySqlServerVersion(new Version(8, 0, 29))
                 );
         }
